d302c20f50ee9f4e887412fa79f25c1c6724d2fb
Victor Grande <vgrandec01@educantabria.es>
2022-05-12 08:50:47 +0200

    Movido título Login a container

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,6 +3,7 @@ import Boton from './Boton';
 import { Container,Form } from 'react-bootstrap'
 import { useHistory } from "react-router-dom";
 import { Users } from "../data/Users";
+
 export default function Login(){
   
   const history = useHistory();
